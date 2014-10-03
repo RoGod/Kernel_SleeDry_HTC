3fe010f44cab2e4faa78b939b3378cb9e389dfcf
rogod <rogersalvarez.ra@gmail.com>
2014-10-03 12:39:00 -0430

    Update

---
+++ busy.sh
@@ -0,0 +1,311 @@
+#!/bin/bash
+#
+
+RAMFS_TMP="/home/rogod/Kernel/tmp/ramfs-source-htc"
+BB="$RAMFS_TMP/sbin/busybox"
+
+ln -s $BB $RAMFS_TMP/sbin/[ >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/[[ >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/acpid >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/add-shell >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/addgroup >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/adduser >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/adjtimex >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ar >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/arp >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/arping >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ash >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/awk >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/base64 >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/basename >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/bbconfig >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/beep >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/blkid >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/blockdev >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/bootchartd >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/brctl >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/bunzip2 >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/bzcat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/bzip2 >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cal >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/catv >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chattr >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chcon > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chgrp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chmod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chown > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chpasswd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chpst > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chroot > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chrt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/chvt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cksum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/clear > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cmp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/comm > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/conspy > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cpio > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/crond > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/crontab > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cryptpw > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cttyhack > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/cut > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/date > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dc > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/deallocvt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/delgroup > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/deluser > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/depmod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/devmem > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/df > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dhcprelay > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/diff > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dirname > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dmesg > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dnsd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dnsdomainname > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dos2unix > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dpkg > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dpkg-dev > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/du > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dumpkmap > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/dumpleases > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/echo > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ed > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/egrep > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/eject > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/env > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/expand > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/expr > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/false > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fbsplash > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fdisk > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fgconsole > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fgrep > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/find > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/findfs > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/flash_lock > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/flash_unlock > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/flashcp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/flock > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fold > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/free > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/freeramdisk > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fstrim > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fsync > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ftpget > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ftpput > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/fuser > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/getenforce > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/getopt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/getsebool > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/grep > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/groups > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/gunzip > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/gzip > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/halt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/head > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/hexdump > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/htop > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/hwclock > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/id > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ifconfig > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/inetd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/insmod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/install > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ionice > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/iostat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ip > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/kill > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/killall > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/killall5 > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/length > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/less > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ln > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/losetup > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ls > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lsattr > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lsmod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lsof > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lspci > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lsusb > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lzcat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lzma > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lzop > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/lzopcat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/makedevs > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/man > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/matchpathcon > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/md5sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mesg > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkdir > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkdosfs > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mke2fs > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkfifo > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkfs.ext2 > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkfs.vfat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mknod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mkswap > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mktemp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/modinfo > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/modprobe > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/more > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mount > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mountpoint > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mpstat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/mv > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nanddump > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nandwrite > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nbd-client > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nc > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/netstat > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nice > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nmeter > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nohup > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/nslookup > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ntpd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/od > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/openvt > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/patch > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pgrep > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pidof > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ping > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ping6 > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pipe_progress > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pkill > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pmap > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/poweroff > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/printenv > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/printf > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ps > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pstree > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pwd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/pwdx > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rdate > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rdev > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/readlink > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/realpath > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/renice > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/reset > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/resize > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/restorecon > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rev > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rm > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rmdir > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rmmod > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/route > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rpm > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rpm2cpio > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rtcwake > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/run-parts > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/runcon > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/runlevel > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/runsv > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/runsvdir > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/rx > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/script > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/scriptreplay > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sed > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/selinuxenabled > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sendmail > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/seq > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sestatus > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setarch > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setconsole > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setenforce > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setfiles > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setserial > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setfont > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setkeycodes > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setsebool > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setlogcons > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setsid > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/setuidgid > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sh > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sha1sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sha256sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sha3sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sha512sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/showkey > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/slattach > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sleep > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/softlimit > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sort > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/split >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/start-stop-daemon >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/stat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/strings > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/stty > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sulogin > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sum > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sv > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/svlogd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/swapoff > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/swapon > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/switch_root > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sync > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sysctl > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/syslogd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/sysreboot > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tac > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tail > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tar > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/taskset > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tcpsvd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tee > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/telnet > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/telnetd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/test > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tftp > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tftpd > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/time > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/timeout > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/top > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/touch > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tr >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/traceroute >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/traceroute6 >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/true >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tty >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/ttysize >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/tune2fs >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/udhcpc >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/udhcpd >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/udpsvd >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/umount >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uname >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uncompress >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unexpand >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uniq >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unix2dos >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unlzma >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unlzop >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unxz >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/unzip >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uptime >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/usleep >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uudecode >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/uuencode >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/vconfig >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/vi >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/vlock >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/volname >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/watch >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/wc > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/wget >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/which >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/who >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/whoami >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/whois >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/xargs >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/xz > /dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/xzcat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/yes >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/zcat >/dev/null 2>&1
+ln -s $BB $RAMFS_TMP/sbin/zcip >/dev/null 2>&1
+
+chmod 750 $RAMFS_TMP/sbin/*
